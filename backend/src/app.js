cfb367e35db1ff49f1f39936010e7175ab0ac405
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-01-30 07:57:56 -0300

    removing body-parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,6 @@
 
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
@@ -17,9 +16,9 @@ const app = express();
 const index = require('./routes/index');
 //ps: declarar a rota userroutes.js
 
-app.use(bodyParser.urlencoded({ extended:true } ) );
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.urlencoded({ extended:true } ) );
+app.use(express.json());
+app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 app.use(morgan('dev'));
 
