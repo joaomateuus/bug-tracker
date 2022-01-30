ed64d59bfc4e364d689706a1745c892a83b63a54
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-01-30 15:07:52 -0300

    login feature

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,15 +1,14 @@
 /**
  *File: userController.js
- *Description: arquivo responsável por listar todas as rotas do user
+ *Description: arquivo controller controla todo fluxo de informaçoes da classe usuário
  *Data: 30/01/2022
  *Author: João Mateus
 */
-
 //importando p usar algumas propiedades neste arquivo
 const User = require('../models/userModel');
 
-//==>Async e await
-//==>Método responsável
+//==>Async e await Método responsável
+
 //metodo do sign in
 exports.signupNewUser = async (req, res) => {
     try {
@@ -35,7 +34,24 @@ exports.signupNewUser = async (req, res) => {
     }
 };
 
-//TODO
-exports.loginUser = async (req, res) =>{};
-//TODO
+//função reponsável pelo login
+exports.loginUser = async (req, res) =>{
+    try {
+        const usuario = req.body.usuario;
+        const senha = req.body.senha;
+        const user = await User.findByCredentials(usuario, senha);
+        
+        //validação de usuário e senha
+        if (!user) {
+            return res.status(401).json({ error: 'Erro ao realizar o Login, verfique se usuário e senha estão corretos ' })
+        }
+
+        const token = await user.generateAuthToken();
+        res.status(201).json({ message: 'Usuário(a) logado com sucesso!!', user, token });
+        
+    } catch (err) {
+        res.status(400).json({ err: err })
+    }
+};
+
 exports.returnUserHome= async (req, res) =>{};
